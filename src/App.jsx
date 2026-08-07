1b88123b617df3a9054b680b66cc08fa1312b380
dev-mohammad78 <dev.mohammad78@gmail.com>
2026-08-07 18:51:33 +0330

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import PageNotFound from "./pages/404";
+
 function App() {
   return (
-    <>
-      <h1 className="color-[var(--text-secondary)] bg-[var(--bg-primary)] text-center text-4xl font-bold p-4">
-        My Shopping Cart Project
-      </h1>
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<DetailsPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
   );
 }
 
