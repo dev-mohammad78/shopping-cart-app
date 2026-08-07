95b622227eed183c34ea4b93ac8f07e4c60a81c4
dev-mohammad78 <dev.mohammad78@gmail.com>
2026-08-07 18:06:29 +0330

    initialized project added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="color-[var(--text-secondary)] bg-[var(--bg-primary)] text-center text-4xl font-bold p-4">
+        My Shopping Cart Project
+      </h1>
+    </>
+  );
+}
+
+export default App;
