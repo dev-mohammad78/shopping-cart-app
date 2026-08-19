c2472980e84e9fd6093b8dd62e332ad53d8a241b
dev-mohammad78 <dev.mohammad78@gmail.com>
2026-08-20 01:39:57 +0330

    footer Layout added

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,14 @@
-import { FiShoppingCart } from "react-icons/fi";
-import { HiMiniShoppingBag } from "react-icons/hi2";
-import { useCart } from "../context/CartContext";
 import { Link, NavLink } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+
+import { FiShoppingCart, FiTruck } from "react-icons/fi";
+import { HiMiniShoppingBag } from "react-icons/hi2";
+import { MdOutlineSecurity } from "react-icons/md";
+import { RiRestartLine } from "react-icons/ri";
+import { TfiHeadphoneAlt } from "react-icons/tfi";
 
 function Layout({ children }) {
   const { state } = useCart();
-  console.log(state);
 
   return (
     <>
@@ -50,8 +53,58 @@ function Layout({ children }) {
           )}
         </Link>
       </header>
+
       {children}
-      <footer>footer</footer>
+
+      <footer className="flex flex-col md:flex-row md:items-center justify-between gap-y-4 bg-[var(--bg-secondary)] px-4 md:px-5 py-5 md:py-9">
+        <div className="flex pr-6 border-r border-[var(--border-primary)]">
+          <div className="border border-[var(--border-primary)] bg-[var(--bg-primary)] rounded-lg p-3">
+            <FiTruck className="text-[var(--primary)] text-3xl font-thin" />
+          </div>
+          <div className="flex flex-col ml-2 mt-1">
+            <h4 className="font-bold text-md">Free Shipping</h4>
+            <p className="text-sm text-[var(--text-secondary)]">
+              On all orders over $50
+            </p>
+          </div>
+        </div>
+
+        <div className="flex pr-6 border-r border-[var(--border-primary)]">
+          <div className="border border-[var(--border-primary)] bg-[var(--bg-primary)] rounded-lg p-3">
+            <MdOutlineSecurity className="text-[var(--primary)] text-3xl" />
+          </div>
+          <div className="flex flex-col ml-2 mt-1">
+            <h4 className="font-bold text-md">Secure Payment</h4>
+            <p className="text-sm text-[var(--text-secondary)]">
+              100% secure payment
+            </p>
+          </div>
+        </div>
+
+        <div className="flex pr-6 border-r border-[var(--border-primary)]">
+          <div className="border border-[var(--border-primary)] bg-[var(--bg-primary)] rounded-lg p-3">
+            <RiRestartLine className="text-[var(--primary)] text-3xl" />
+          </div>
+          <div className="flex flex-col ml-2 mt-1">
+            <h4 className="font-bold text-md">30 Days Return</h4>
+            <p className="text-sm text-[var(--text-secondary)]">
+              Money back guarantee
+            </p>
+          </div>
+        </div>
+
+        <div className="flex pr-6">
+          <div className="border border-[var(--border-primary)] bg-[var(--bg-primary)] rounded-lg p-3">
+            <TfiHeadphoneAlt className="text-[var(--primary)] text-3xl" />
+          </div>
+          <div className="flex flex-col ml-2 mt-1">
+            <h4 className="font-bold text-md">24/7 Support</h4>
+            <p className="text-sm text-[var(--text-secondary)]">
+              Dedicated support
+            </p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
